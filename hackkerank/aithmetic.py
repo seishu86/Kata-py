f1cfa7d6b1651ddf565c64cf06fb4a95a31eeb52
Khal <itzshakeey@gmail.com>
2021-12-22 23:03:52 +0000

    arithmetic problem

---
+++ hackkerank/aithmetic.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    sum = a+b
+    print(sum)
+    
+    difference = a-b
+    print(difference)
+    
+    product = a*b
+    print(product)
